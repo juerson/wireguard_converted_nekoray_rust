0f136b5e96867cb40f786a2413baa4b44dc6056d
Juerson <37030166+juerson@users.noreply.github.com>
2023-10-04 05:14:01 +0800

    修改生成IPv6链接的问题

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@ extern crate base64;
 use regex::Regex;
 use std::fs;
 use std::io::{self, Write};
+use std::net::Ipv6Addr;
+use std::net::Ipv4Addr;
+use lazy_static::lazy_static;
 
+
+// 使用 lazy_static 宏创建静态正则表达式对象，使用 lazy_static 宏可以在第一次使用正则表达式时初始化它们，以后就不需要再次编译。
+lazy_static! {
+    static ref IPV4_REGEX: Regex = Regex::new(r#"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"#).unwrap();
+    static ref IPV6_REGEX: Regex = Regex::new(r#"^(?:(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:))$"#).unwrap();
+}
 
 fn check_file_exist_or_zero_size(file: &str) {
     if !fs::metadata(file).is_ok() || fs::metadata(file).unwrap().len() == 0 {
@@ -72,7 +81,7 @@ fn wait_for_enter() {
 fn main() {
     let file = "wg-config.conf";
     check_file_exist_or_zero_size(file);
-	println!("本程序可以将WireGuard配置文件的信息转为NekoRay节点！\n");
+	println!("本程序可以将WARP的WireGuard配置文件的信息转为NekoRay节点！\n");
     let mut input_mut = String::new();
     loop {
         print!("是否修改MTU值(默认是配置文件中的值，可用的取值范围1280~1500)：");
@@ -92,21 +101,44 @@ fn main() {
 	println!("\n{:+<101}", "");
 	loop {
 		let mut input_endpoint = String::new();
-		let re = Regex::new(r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:[0-9]+)?$").unwrap();
+		let mut ip;
+		let mut port;
 		loop {
-			print!("输入优选IP(格式：162.159.192.1:2408)：");
+			print!("\n输入优选IP(格式：162.159.192.1:2408)：");
 			io::stdout().flush().expect("无法刷新标准输出缓冲区");
 			input_endpoint.clear();
 			io::stdin().read_line(&mut input_endpoint).expect("无法读取输入");
 			input_endpoint = input_endpoint.trim().to_string();
-			if re.is_match(&input_endpoint) {
-				break;
+			let mut parts: Vec<&str> = Vec::new();
+
+			if input_endpoint.starts_with('[') {
+				if let Some(end_idx) = input_endpoint.find(']') {
+					parts.push(&input_endpoint[1..end_idx]);
+					if let Some(port_idx) = input_endpoint[end_idx + 1..].find(':') {
+						parts.push(&input_endpoint[end_idx + 2 + port_idx..]);
+					}
+				}
+			} else if let Some(port_idx) = input_endpoint.find(':') {
+				parts.push(&input_endpoint[..port_idx]);
+				parts.push(&input_endpoint[port_idx + 1..]);
+			}
+
+			if parts.len() == 2 {
+				ip = parts[0];
+				port = parts[1];
+				if IPV4_REGEX.is_match(ip) {
+				if let Ok(_ipv4) = ip.parse::<Ipv4Addr>() {
+						break
+					}
+				} else if IPV6_REGEX.is_match(ip) {
+					if let Ok(_ipv6) = ip.parse::<Ipv6Addr>() {
+						break
+					}
+				}
+			} else {
+				
 			}
 		}
-		
-		let ip_port: Vec<&str> = input_endpoint.split(":").collect();
-		let ip = ip_port[0].trim();
-		let port = ip_port[1].trim();
 
 		let mut input_country = String::new();
 		print!("添加节点名称或别名的前缀吗？(比如，CN)：");
@@ -118,8 +150,13 @@ fn main() {
 		} else {
 			"".to_string()
 		};
-		
-		let node = base_str.replace("{name}", &format!("{}{}:{}", country, ip, port)).replace("{server}", ip).replace("{server_port}", port);
+		let host_name;
+		if ip.contains(':') {
+			host_name = format!("[{}]",ip);
+		} else {
+			host_name = ip.to_string();
+		};
+		let node = base_str.replace("{name}", &format!("{}{}:{}", country, host_name, port)).replace("{server}", ip).replace("{server_port}", port);
 		let encoded = base64::encode(node);
 		let transport_protocol = "nekoray://custom#";
 		let nekoray_node = format!("{}{}", transport_protocol, encoded);
@@ -127,7 +164,7 @@ fn main() {
 		println!("\n{:-<43}NekoRay节点如下{:-<43}", "", "");
 		println!("{}", nekoray_node);
 		println!("{:-<101}", "");
-		println!("节点已经生成好，将上面的NekoRay链接复制到NekoBox软件使用即可！记得要切换为sing-box核心。\n");
+		println!("\n节点已经生成好，将上面的NekoRay链接复制到NekoBox软件使用即可！记得要切换为sing-box核心。\n");
 		println!("{:+<101}", "");
 	}
 }
